02f462a14cdbf374f7fee6533e1816642c67dbdb
katinbox <tranquanki28@gmail.com>
2022-12-09 09:56:37 +0700

    fix: duplidate delared InviteList component

--- src/pages/ProjectListPage.js
+++ src/pages/ProjectListPage.js
@@ -1,35 +1,34 @@
-import React from 'react'
-import {useState, useEffect} from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import '../assets/scss/ProjectListPage/main.scss'
-import InviteList from '../components/InviteList/InviteList'
-import ProjectList from '../components/ProjectList/ProjectList'
-import SearchBar from '../components/SearchBar/SearchBar'
-import ProjectServices from '../services/Project/projectServices';
+import '../assets/scss/ProjectListPage/main.scss';
 import InviteList from '../components/InviteList/InviteList';
+import ProjectList from '../components/ProjectList/ProjectList';
+import SearchBar from '../components/SearchBar/SearchBar';
+import ProjectServices from '../services/Project/projectServices';
 import ProjectForm from '../components/ProjectModal/ProjectModal';
 import Overlay from '../components/Overlay/Overlay';
 import RoleForm from '../components/RoleForm/RoleForm';
 
 const ProjectListPage = () => {
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState('')
-    const [overlay, setOverlay] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [overlay, setOverlay] = useState(false);
     const [openProject, setOpenProject] = useState(false);
 
     //const projectData = useSelector(state => state.projectData)
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        getProject()
-    }, [])
+        getProject();
+    }, []);
 
     const getProject = async () => {
-        setLoading(true)
+        setLoading(true);
         try {
-            const getProjectResponse = await ProjectServices.getProject()
-            dispatch({ 'type':'setData', 'value': [...getProjectResponse.data]})
-            setLoading(false)
+            const getProjectResponse = await ProjectServices.getProject();
+            dispatch({ type: 'setData', value: [...getProjectResponse.data] });
+            setLoading(false);
         } catch (err) {
             if (Array.isArray(err.response.data)) {
                 const errorResponse = err.response.data;
@@ -39,27 +38,29 @@ const ProjectListPage = () => {
                 setError(err.response.data.msg);
             }
         }
-    }
+    };
 
     const handleRoleButton = () => {
         setOverlay(!overlay);
-    }
+    };
 
-  return (
-      <>
-          {overlay&&<Overlay overlay={overlay} handleRoleButton={handleRoleButton}>
-              <RoleForm/>
-          </Overlay>}
-          <div className='project-list-page'>
-              <InviteList/>
-              <div className='pjs-container'>
-                  <SearchBar modal={{ setOpenProject }}/>
-                  <ProjectList  overlay={overlay} handleRoleButton={handleRoleButton} loading={loading}/>
-                  <ProjectForm modal={{ openProject, setOpenProject }} />
-              </div>
-          </div>
-      </>
-  )
-}
+    return (
+        <>
+            {overlay && (
+                <Overlay overlay={overlay} handleRoleButton={handleRoleButton}>
+                    <RoleForm />
+                </Overlay>
+            )}
+            <div className="project-list-page">
+                <InviteList />
+                <div className="pjs-container">
+                    <SearchBar modal={{ setOpenProject }} />
+                    <ProjectList overlay={overlay} handleRoleButton={handleRoleButton} loading={loading} />
+                    <ProjectForm modal={{ openProject, setOpenProject }} />
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default ProjectListPage;
