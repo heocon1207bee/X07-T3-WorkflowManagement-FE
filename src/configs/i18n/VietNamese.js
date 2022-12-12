aee7dbd682bfb10122dee3d16619f91aed5b4cdb
katinbox <tranquanki28@gmail.com>
2022-12-12 11:29:55 +0700

    feat: add capability vietnamese

--- src/configs/i18n/VietNamese.js
+++ src/configs/i18n/VietNamese.js
@@ -5,3 +5,12 @@ export const PROJECT_MEMBER_VN = 'Thành viên';
 export const PROJECT_IN_PROGRESS_VN = 'Đang thực hiện';
 export const PROJECT_DONE_VN = 'Hoàn thành';
 export const PROJECT_CANCEL_VN = 'Đã hủy';
+
+export const CREATE_CARD_VN = 'Tạo công việc';
+export const DELETE_CARD_VN = 'Hủy bỏ công việc';
+export const MANAGE_MEMBER_VN = 'Quản lý thành viên';
+export const MANAGE_ROLE_VN = 'Quản lý vai trò';
+export const UPDATE_ASSIGNEE_VN = 'Cập nhật người thực hiện công việc';
+export const UPDATE_CARD_STATUS_VN = 'Cập nhật trạng thái công việc';
+export const UPDATE_CARD_VN = 'Cập nhật nội dung công việc';
+export const UPDATE_PROJECT_VN = 'Cập nhật nội dung dự án';
