8ea75d6930620ed5642bc676f1c7520292feb112
katinbox <tranquanki28@gmail.com>
2023-01-08 21:26:01 +0700

    refact: replace magic string with const

---
+++ src/configs/CARD_STATUS.js
@@ -0,0 +1,6 @@
+export const CARD_OPEN = 'CARD_OPEN';
+export const CARD_INPROGRESS = 'CARD_INPROGRESS';
+export const CARD_PREVIEW = 'CARD_PREVIEW';
+export const CARD_DONE = 'CARD_DONE';
+export const CARD_REOPEN = 'CARD_REOPEN';
+export const CARD_CANCEL = 'CARD_CANCEL';
