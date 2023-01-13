7f9c147a0c6ce567ce551ed4338b9f7c177e51c2
katinbox <tranquanki28@gmail.com>
2023-01-13 10:27:23 +0700

    style: icon card item

--- src/components/TaskDetails/TaskInfo.js
+++ src/components/TaskDetails/TaskInfo.js
@@ -1,11 +1,29 @@
 import React, { useRef } from 'react';
 import { FiEdit } from 'react-icons/fi';
 import dayjs from 'dayjs';
-import { FcAlarmClock, FcBriefcase, FcHighPriority, FcLowPriority, FcMediumPriority, FcVlc } from 'react-icons/fc';
+import {
+    FcAlarmClock,
+    FcBriefcase,
+    FcHighPriority,
+    FcLowPriority,
+    FcMediumPriority,
+    FcOk,
+    FcOpenedFolder,
+    FcProcess,
+    FcRefresh,
+    FcRegisteredTrademark,
+    FcSearch,
+    FcVlc,
+} from 'react-icons/fc';
 
 import { CARD_TASK, CARD_ISSUE } from '../../configs/CARD_TYPES';
 import {
+    CARD_DONE_VN,
+    CARD_IN_PREVIEW_VN,
+    CARD_IN_PROGRESS_VN,
     CARD_ISSUE_VN,
+    CARD_OPEN_VN,
+    CARD_RE_OPEN_VN,
     CARD_TASK_VN,
     PRIORITY_HIGHEST_VN,
     PRIORITY_HIGH_VN,
@@ -22,6 +40,7 @@ import {
 } from '../../configs/PRIORITIES';
 
 import { Tooltip } from 'antd';
+import { CARD_DONE, CARD_IN_PREVIEW, CARD_IN_PROGRESS, CARD_OPEN, CARD_RE_OPEN } from '../../configs/CARD_STATUS';
 
 const cardType = {
     [CARD_TASK]: {
@@ -56,8 +75,30 @@ const priorityType = {
         icon: <FcLowPriority />,
     },
 };
+const cardStatus = {
+    [CARD_OPEN]: {
+        label: CARD_OPEN_VN,
+        icon: <FcOpenedFolder />,
+    },
+    [CARD_RE_OPEN]: {
+        label: CARD_RE_OPEN_VN,
+        icon: <FcRefresh />,
+    },
+    [CARD_IN_PROGRESS]: {
+        label: CARD_IN_PROGRESS_VN,
+        icon: <FcProcess />,
+    },
+    [CARD_IN_PREVIEW]: {
+        label: CARD_IN_PREVIEW_VN,
+        icon: <FcSearch />,
+    },
+    [CARD_DONE]: {
+        label: CARD_DONE_VN,
+        icon: <FcOk />,
+    },
+};
 const TaskInfo = ({ card }) => {
-    const { title, type, priority, assignee, deadline, description } = card;
+    const { title, type, priority, assignee, deadline, description, status } = card;
 
     return (
         <div className="task-info-container">
@@ -71,22 +112,25 @@ const TaskInfo = ({ card }) => {
             </div>
             <div className="task-info">
                 <p>
-                    <b>Loại:</b> {cardType[type].icon} {cardType[type].label}
+                    <b>Loại: </b> {cardType[type].icon} {cardType[type].label}
                 </p>
                 <p>
-                    <b>Độ ưu tiên:</b> {priorityType[priority].icon} {priorityType[priority].label}
+                    <b>Độ ưu tiên: </b> {priorityType[priority].icon} {priorityType[priority].label}
                 </p>
                 <p>
-                    <b>Người thực hiện:</b> {assignee?.fullname}
+                    <b>Người thực hiện: </b> {assignee?.fullname}
                 </p>
                 <p>
-                    <b>Ngày hết hạn:</b>
+                    <b>Trạng thái: </b> {cardStatus[status].icon} {cardStatus[status].label}
+                </p>
+                <p>
+                    <b>Ngày hết hạn: </b>
                     <FcAlarmClock /> {dayjs(deadline).format('DD-MM-YYYY')}
                 </p>
-                <p>
-                    <b>Mô tả công việc</b>
-                </p>
             </div>
+            <p>
+                <b>Mô tả công việc</b>
+            </p>
             <div dangerouslySetInnerHTML={{ __html: description }}></div>
         </div>
     );
--- src/components/TaskList/TaskItem.js
+++ src/components/TaskList/TaskItem.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { formatDate } from '../../utils/DateTimeFormater';
 import TaskDetails from '../TaskDetails/TaskDetails';
-import { CARD_OPEN } from '../../configs/CARD_STATUS';
 import { CARD_TASK, CARD_ISSUE } from '../../configs/CARD_TYPES';
 import {
     PRIORITY_HIGHEST,
@@ -17,27 +16,52 @@ import {
     PRIORITY_MEDIUM_VN,
     PRIORITY_LOW_VN,
     PRIORITY_LOWEST_VN,
-    CARD_OPEN_VN,
     CARD_ISSUE_VN,
     CARD_TASK_VN,
 } from '../../configs/i18n/VietNamese';
+import { FcBriefcase, FcHighPriority, FcLowPriority, FcMediumPriority, FcVlc } from 'react-icons/fc';
 
 const card_trans = {
     [CARD_TASK]: CARD_TASK_VN,
     [CARD_ISSUE]: CARD_ISSUE_VN,
 };
 
+const priorityType = {
+    [PRIORITY_HIGHEST]: {
+        label: PRIORITY_HIGHEST_VN,
+        icon: <FcHighPriority />,
+    },
+    [PRIORITY_HIGH]: {
+        label: PRIORITY_HIGH_VN,
+        icon: <FcHighPriority />,
+    },
+    [PRIORITY_MEDIUM]: {
+        label: PRIORITY_MEDIUM_VN,
+        icon: <FcMediumPriority />,
+    },
+    [PRIORITY_LOW]: {
+        label: PRIORITY_LOW_VN,
+        icon: <FcLowPriority />,
+    },
+    [PRIORITY_LOWEST]: {
+        label: PRIORITY_LOWEST_VN,
+        icon: <FcLowPriority />,
+    },
+};
+const cardType = {
+    [CARD_TASK]: {
+        label: CARD_TASK_VN,
+        icon: <FcBriefcase />,
+    },
+    [CARD_ISSUE]: {
+        label: CARD_ISSUE_VN,
+        icon: <FcVlc />,
+    },
+};
+
 const TaskItem = (props) => {
     const [openDetail, setOpenDetail] = useState(false);
     const themeStore = useSelector((state) => state.theme);
-    const status = (status) => {
-        switch (status) {
-            case CARD_OPEN:
-                return CARD_OPEN_VN;
-            default:
-                return '';
-        }
-    };
     const priorityTrans = (priority) => {
         switch (priority) {
             case PRIORITY_HIGHEST:
@@ -72,6 +96,10 @@ const TaskItem = (props) => {
         }
     };
 
+    const isTask = () => {
+        return props.task.type === CARD_TASK;
+    };
+
     return (
         <>
             <div
@@ -81,31 +109,26 @@ const TaskItem = (props) => {
                 onDragEnd={props.onDragEnd}
                 draggable
             >
-                <div className='task-item-title'>
+                <div className="task-item-title">
                     {props.task.title} {/*-<span>{status(props.task.status)}</span>*/}
                 </div>
-                <div className='task-item-info'>
-                    <div
-                        className={
-                            props.task.type === CARD_TASK ? 'task-item-type task-status' : 'task-item-type issue-status'
-                        }
-                    >
-                        {card_trans[props.task.type]}
+                <div className="task-item-info">
+                    <div className={isTask() ? 'task-item-type task-status' : 'task-item-type issue-status'}>
+                        {cardType[props.task.type].icon} {card_trans[props.task.type]}
                     </div>
                     <div className={'task-item-priority ' + priority(props.task.priority)}>
-                        {priorityTrans(props.task.priority)}
+                        {priorityType[props.task.priority].icon} {priorityTrans(props.task.priority)}
                     </div>
-                    <div className='task-item-create'>
+                    <div className="task-item-create">
                         Người tạo: {props.task.owner.fullname ? props.task.owner.fullname : 'Không dữ liệu'}
                     </div>
-                    <div className='task-item-do'>
+                    <div className="task-item-do">
                         Người thực hiện: {props.task.assignee.fullname ? props.task.assignee.fullname : 'Không dữ liệu'}
                     </div>
-                    <div className='task-item-deadline'>Hết hạn: {formatDate(props.task.deadline)}</div>
+                    <div className="task-item-deadline">Hết hạn: {formatDate(props.task.deadline)}</div>
                 </div>
             </div>
             {openDetail && <TaskDetails open={openDetail} setClose={() => setOpenDetail(false)} card={props.task} />}
-
         </>
     );
 };
